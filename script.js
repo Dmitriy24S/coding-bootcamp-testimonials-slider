d560f1dff16677a464b63736958c8c56af90f177
Dmitriy <devilins24@gmail.com>
2022-03-24 18:08:23 +0200

    style padding adjustment, js slider loop func adjust.

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const nextBtns = document.querySelectorAll(".next-arrow");
 const prevBtns = document.querySelectorAll(".prev-arrow");
-const reviews = document.querySelectorAll(".text-section");
+const reviews = Array.from(document.querySelectorAll(".text-section"));
+
+let index = 1;
 
 const changeActiveProfile = () => {
+  // clear active review
   reviews.forEach((review) => {
-    review.classList.toggle("active");
+    review.classList.remove("active");
   });
+  // add new active review
+  reviews[index].classList.add("active");
 };
 
 nextBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
+    if (index >= reviews.length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
     changeActiveProfile();
   });
 });
 
 prevBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
+    if (index === 0) {
+      index = reviews.length - 1;
+    } else {
+      index--;
+    }
     changeActiveProfile();
   });
 });
